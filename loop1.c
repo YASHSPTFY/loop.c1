db78a7266b760d92d4704443890f9d4c17933da0
YASHSPTFY <92641420+YASHSPTFY@users.noreply.github.com>
2021-10-25 01:42:13 +0530

    Add files via upload

---
+++ loop1.c
@@ -0,0 +1,40 @@
+/*#include<stdio.h> 
+int main()
+{ 
+  int i, j;
+  
+ for(i=0;i<5;++i)
+ { 
+   for(j=0;j<=i;++j)
+   {
+    printf("*");
+	 } 
+	 printf("\n");
+	  }
+	   return 0; 
+	   }*/
+	   
+#include<stdio.h>
+int main()
+{
+	int i,j;
+	
+	for(i=0;i<15;++i)
+	{
+		for(j=0;j<=i;++j)
+		{
+			printf("01");
+		}
+		
+		printf("\n");
+	}
+	return 0;
+}
+	
+	
+	
+	
+	
+	
+	
+	
